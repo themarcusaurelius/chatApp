83be74d1f2b5678e31b0c97e143765d70ac08932
Marcus Aurelius <markmayfield16@gmail.com>
2019-06-08 18:12:06 -0500

    Create Modal For Adding Channels

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,23 +1,70 @@
-import React from 'react';
-import { Menu, Icon } from 'semantic-ui-react';
+import React, { Fragment } from 'react';
+import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
 
 class Channels extends React.Component {
     state = {
-        channels: []
-    }
+        channels: [],
+        channelName: '',
+        channelDetails: '',
+        modal: false
+    };
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    };
+
+    openModal = () => this.setState({ modal: true });
+
+    closeModal = () => this.setState({ modal: false });
 
     render() {
-        const { channels } = this.state;
+        const { channels, modal } = this.state;
 
         return (
-            <Menu.Menu style={{ paddingBottom: '2em'}}>
-                <Menu.Item>
-                    <span>
-                        <Icon name="exchange"/> CHANNELS
-                    </span>
-                    ({ channels.length }) <Icon name="add"/>
-                </Menu.Item>
-            </Menu.Menu>
+            <Fragment>
+                <Menu.Menu style={{ paddingBottom: '2em'}}>
+                    <Menu.Item>
+                        <span>
+                            <Icon name="exchange"/> CHANNELS
+                        </span>{' '}
+                        ({ channels.length }) <Icon name="add" onClick={this.openModal}/>
+                    </Menu.Item>
+                </Menu.Menu>
+
+                <Modal basic open={modal} onClose={this.closeModal}>
+                    <Modal.Header>Add a Channel</Modal.Header>
+                    <Modal.Content>
+                        <Form>
+                            <Form.Field>
+                                <Input 
+                                    fluid
+                                    label="Channel Name"
+                                    name="channelName"
+                                    onChange={this.handleChange}
+                                />
+                            </Form.Field>
+
+                            <Form.Field>
+                                <Input 
+                                    fluid
+                                    label="Channel Details"
+                                    name="channelDetails"
+                                    onChange={this.handleChange}
+                                />
+                            </Form.Field>
+                        </Form>
+                    </Modal.Content>
+
+                    <Modal.Actions>
+                        <Button color="green" inverted>
+                            <Icon name="checkmark" /> Add
+                        </Button>
+                        <Button color="red" inverted onClick={this.closeModal}>
+                            <Icon name="remove" /> Cancel
+                        </Button>
+                    </Modal.Actions>
+                </Modal>
+            </Fragment>
         );
     }
 };
