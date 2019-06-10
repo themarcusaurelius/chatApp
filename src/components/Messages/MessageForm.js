9a3742e7b055161ad6f333f46326e1b8dabcdea7
Marcus Aurelius <markmayfield16@gmail.com>
2019-06-09 22:59:17 -0500

    Displaying Progress Bar

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -4,6 +4,7 @@ import firebase from '../../firebase';
 import { Segment, Button, Input } from 'semantic-ui-react';
 
 import FileModal from './FileModal';
+import ProgressBar from './ProgressBar';
 
 class MessageForm extends React.Component {
     state = {
@@ -131,11 +132,18 @@ class MessageForm extends React.Component {
     }
 
     render() {
-        const { errors, message, loading, modal } = this.state
+        const { 
+            errors, 
+            message, 
+            loading, 
+            modal, 
+            uploadState, 
+            percentUploaded 
+        } = this.state
 
         return (
-           <Segment className="message__form">
-               <Input 
+            <Segment className="message__form">
+                <Input 
                     fluid
                     name="message"
                     onChange={this.handleChange}
@@ -149,30 +157,34 @@ class MessageForm extends React.Component {
                             : ''
                     }
                     placeholder="Write your message"
-               />
-               <Button.Group icon widths="2">
-                   <Button
+                />
+                <Button.Group icon widths="2">
+                    <Button
                         onClick={this.sendMessage}
                         disabled={loading}
                         color="orange"
                         content="Add Reply"
                         labelPosition="left"
                         icon="edit"
-                   />
-                   <Button 
+                    />
+                    <Button 
                         color="teal"
                         onClick={this.openModal}
                         content="Upload Media"
                         labelPosition="right"
                         icon="cloud upload"
-                   />
-                   <FileModal 
-                        modal={modal}
-                        closeModal={this.closeModal}
-                        uploadFile={this.uploadFile}
                     />
-               </Button.Group>
-           </Segment> 
+                </Button.Group>
+                <FileModal 
+                    modal={modal}
+                    closeModal={this.closeModal}
+                    uploadFile={this.uploadFile}
+                />
+                <ProgressBar 
+                    uploadState={uploadState}
+                    percentUploaded={percentUploaded}
+                />
+            </Segment> 
         );
     }
 };
